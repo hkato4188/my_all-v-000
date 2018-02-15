95265fc2d82b1b7288eb68efb7f0f80b3f9fd7ec
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-15 14:13:50 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,11 +3,13 @@ require 'pry'
 def my_all(collection)
   new_array = []
   i = 0
+
   while i < collection.length
     new_array << yield(collection[i])
     i += 1
     end
-  if new_array.include(false)
+
+  if new_array.include?(false)
     false
   else
     true
